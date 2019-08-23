3d276da886be9ea86f48c89210fd71d2a3427cfb
chasegiles2 <chasegiles2@gmail.com>
2019-08-23 11:27:13 -0500

    added view definition for monitor_hour_threshold

---
+++ SQL_Files/monitor_hour_threshold.sql
@@ -0,0 +1,16 @@
+Create or replace view auctions.monitor_hour_threshold as
+--use this query to set the threshold by hour for the monitoring
+Select auction_hour, min(difference), avg(difference), max(difference) from
+(
+	Select 
+	auction_time,
+	extract(HOUR from auction_time) as auction_hour,
+	auction_time - lag(auction_time) over (order by auction_time) as difference from
+	(
+	Select max(retrieval_time) as auction_time from auctions.bid_history
+	Group by auction_id
+	Order by max(retrieval_time)
+	) auction_end
+) y
+Where difference < '06:00:00' --removes outliers caused by outages
+Group by auction_hour
